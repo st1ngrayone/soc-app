f62ab33fe246ec45907bfe86df67ddb4e19b9ad2
st1ngray <user-13@example.com>
2021-04-16 12:12:34 +0300

    commit 4

--- application/routes.py
+++ application/routes.py
@@ -11,8 +11,17 @@ from application import app
 @login_required
 def users():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    users = cursor.execute('SELECT * FROM accounts')
-    account = cursor.fetchone()
+    cursor.execute('SELECT name, lastname, city FROM accounts WHERE name is not NULL')
+    users = cursor.fetchall()
+    return render_template('users.html',users=users)
+
+@app.route('/friends')
+@login_required
+def users():
+    userid = cursor.fetchone()
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT U.username, U.email FROM accounts U, friends F WHERE CASE WHEN F.friend_one = 4 THEN F.friend_two = U.id WHEN F.friend_two = 5 THEN F.friend_one= U.id END'), (userid, )
+    users = cursor.fetchall()
     return render_template('users.html',users=users)
 
 
